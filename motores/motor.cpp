d0fbe4f381bd7e4b05c2e328d092dcd3d5244a3a
wrtemis <iol.davidias@gmail.com>
2024-04-29 17:23:35 -0300

    arrumei uns erros do motor

--- motores/motor.cpp
+++ motores/motor.cpp
@@ -1,21 +1,17 @@
 #include "motor.h"
 
 Motor::Motor(const int pinA):_pin1(pinA),_speed(130) { //inicializando _pin com valor pin e _speed com valor 130
-
-    pinMode(_pin1, OUTPUT); //definindo como entrada
-    digitalWrite(_pinA, LOW); //desligado
-}
-
-void Motor::motorOn() {
-    digitalWrite(_pinA, HIGH);
-}
-
-void Motor::motorOff() {
-    digitalWrite(_pinA, LOW);
+    this->pinA = pinA;
+    this->speed = 130; 
 }
 
 void Motor::motor1Speed(int speed) {
 
-    _speed = speed;
-    analogWrite(_pinA, _speed);
+    this->speed = speed;
+    analogWrite(pinA, speed);
+}
+
+Motor::update(){
+    this->statePin1 = digitalRead(this->statePin1);
+    this->statePin2 = digitalRead(this->statePin2);
 }
