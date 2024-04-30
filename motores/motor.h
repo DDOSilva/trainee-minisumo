33fa7a7ac8d2d95abe5cc12110e72d135a2c3546
wrtemis <iol.davidias@gmail.com>
2024-04-30 14:05:48 -0300

    motor ok

--- motores/motor.h
+++ motores/motor.h
@@ -6,14 +6,14 @@ class Motor {
     public:
         int speed;
         int pin_pwm;
-        int pin_Ch;
+        int pin_ch;
         int pin_1;
         int pin_2; 
         int cSpeed; //constant speed
         int speed_max = 100;
 
         void update ();
-        Motor (int pin_ch, int pin_1, int pin_2 int pin_pwm, int speed);
+        Motor (int pin_ch, int pin_1, int pin_2, int pin_pwm, int speed);
         void setspeed(int speed);
 
 };
