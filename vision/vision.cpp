8d4d278b541c7006fc21ee7e78c4ea151bc35dcb
JVFS2005 <joaovitorloucutor@hotmail.com>
2024-05-01 19:17:24 -0300

    visao ok?

--- vision/vision.cpp
+++ vision/vision.cpp
@@ -1,31 +1,15 @@
 #include "vision.h"
 #include "digital_sensor.h"
 
-Vision::Vision(int pin_fl, int pin_l, int pin_f, int pin_r, int pin_fr){
-    this->pin_fl = pin_fl;
-    this->pin_l = pin_l;
-    this->pin_f = pin_f;
-    this->pin_r = pin_r;
-    this->pin_fr = pin_fr;
-}
+Vision::Vision(){};
 
 void Vision::update(){
-    DigitalSensor sensor_fl(this->pin_fl);
-    DigitalSensor sensor_l(this->pin_l);
-    DigitalSensor sensor_f(this->pin_f);
-    DigitalSensor sensor_r(this->pin_r);
-    DigitalSensor sensor_fr(this->pin_fr);
-
-    sensor_fl.update();
-    sensor_l.update();
-    sensor_f.update();
-    sensor_r.update();
-    sensor_fr.update();
-
-    if (sensor_f.state) enemy_position = "Front", last_position = "Front";
-    else if(sensor_l.state) enemy_position = "Left", last_position = "Left";
-    else if(sensor_r.state) enemy_position = "Right", last_position = "Right";
-    else if(sensor_fl.state) enemy_position = "Full Left", last_position = "Full Left";
-    else if(sensor_fr.state) enemy_position = "Full Right", last_position = "Full Right";
-    else enemy_position = "None";
+    if (sensor_f.state) enemy_position = EnemyPosition::FRONT, latest_position = EnemyPosition::FRONT, return;
+    else if(sensor_l.state) enemy_position = EnemyPosition::LEFT, latest_position = EnemyPosition::LEFT, return;
+    else if(sensor_r.state) enemy_position = EnemyPosition::RIGHT, latest_position = EnemyPosition::RIGHT, return;
+    else if(sensor_fl.state) enemy_position = EnemyPosition::FULL_LEFT, latest_position = EnemyPosition::FULL_LEFT, return;
+    else if(sensor_fr.state) enemy_position = EnemyPosition::FULL_RIGHT, latest_position = EnemyPosition::FULL_RIGHT, return;
+    else if(latest_position == EnemyPosition::FULL_LEFT || latest_position == EnemyPosition::LEFT) enemy_position = EnemyPosition::SEARCH_LEFT, return;
+    else if(latest_position == EnemyPosition::FULL_RIGHT || latest_position == EnemyPosition::RIGHT) enemy_position = EnemyPosition::SEARCH_RIGHT, return;
+    else enemy_position = EnemyPosition::SEARCH_RIGHT, return;
 }
--- vision/vision.h
+++ vision/vision.h
@@ -1,15 +1,27 @@
 #ifndef VISION_H
 #define VISION_H
-#include <string>
+#include "digital_sensor.h"
+
+namespace EnemyPosition {
+    enum position {
+        FRONT, // 0
+        LEFT, // 1
+        FULL_LEFT, //2
+        RIGHT, //3
+        FULL_RIGHT, //4
+        SEARCH_LEFT, //5
+        SEARCH_RIGHT //6
+    };
+};
 
 class Vision{
     public:
-    int pin_fl, pin_l, pin_f, pin_r, pin_fr;
-    std::string enemy_position, last_position;
 
-    Vision(int pin_fl, int pin_l, int pin_f, int pin_r, int pin_fr);
+    EnemyPosition::position enemy_position, latest_position;
 
-    void update();
+    Vision();
+
+    void update(DigitalSensor sensor_l, DigitalSensor sensor_fl, DigitalSensor sensor_r, DigitalSensor sensor_fr, DigitalSensor sensor_f);
 
 };
 
