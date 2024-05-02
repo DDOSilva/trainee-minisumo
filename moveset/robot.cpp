445e7e728944fb48f21147eb4d5655a833bda987
Rodrigo Bon <106597452+RodboN@users.noreply.github.com>
2024-05-02 10:40:30 -0300

    atz1

--- moveset/robot.cpp
+++ moveset/robot.cpp
@@ -7,7 +7,28 @@
 #include "microstart.h"
 
 
-Moveset::Moveset() {};
+
+Moveset::Moveset() : rightMotor(RIGHT_MOTOR_PWM, RIGHT_MOTOR_PIN1, ...),
+                        f(SENSOR_F),
+                        uStart(MICRIS_STARTER...),
+{
+        this->... = MICROSTART::STOP;
+}
+
+void ...::readsensor(){
+        this->...sensor.updat();
+        .
+        .
+        .
+        .
+}
+
+void luta..(){
+        this->microstart.update();
+        this->readsensor();
+        this->vision.updateEnemyPsition(this->f, this->...);
+        this->update();
+
 
 void Moveset::update(Motor &rightMotor, Motor &leftMotor, Vision &vision, Microstarter &uStart, DigitalSensor &sensor_l
         DigitalSensor &sensor_r, DigitalSensor &sensor_fl, DigitalSensor &sensor_fr, DigitalSensor &sensor_f) {
@@ -42,4 +63,43 @@ void Moveset::update(Motor &rightMotor, Motor &leftMotor, Vision &vision, Micros
         return;
     }
 
+}
+
+
+
+
+
+//criar uma main separada para pinmode e loop de luta
+
+setup {
+        Serial.begin(115200);
+pinMode(FRONT_PIN, INPUT);
+  pinMode(FULL_RIGHT_PIN, INPUT);
+  pinMode(FULL_LEFT_PIN, INPUT);
+  pinMode(LEFT_PIN, INPUT);
+  pinMode(RIGHT_PIN, INPUT);
+
+  pinMode(RIGHT_MOTOR_PWM_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN_PIN_1, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN_PIN_2, OUTPUT);
+  pinMode(LEFT_MOTOR_PWM_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_IN_PIN_1, OUTPUT);
+  pinMode(LEFT_MOTOR_IN_PIN_2, OUTPUT);
+
+  pinMode(MICRO_START_INPUT_PIN, INPUT);
+
+  // pinMode(STDBY, OUTPUT);
+  // digitalWrite(STDBY, HIGH);
+
+  ledcAttachPin(RIGHT_MOTOR_PWM_PIN, RIGHT_MOTOR_CHANNEL);
+  ledcSetup(RIGHT_MOTOR_CHANNEL, 12000, 8);
+
+  ledcAttachPin(LEFT_MOTOR_PWM_PIN, LEFT_MOTOR_CHANNEL);
+  ledcSetup(LEFT_MOTOR_CHANNEL, 12000, 8);
+        ...
+        }
+
+loop{
+        static Robot robo;
+        robo.luta();
 }
