ba2d741c05d195cbde3fe762ce2c6a671c008670
wrtemis <iol.davidias@gmail.com>
2024-05-05 12:15:07 -0300

    microstart corrigido

--- microstart/microstart.cpp
+++ microstart/microstart.cpp
@@ -4,14 +4,15 @@
 
 Microstarter::Microstarter(int pin) {
   //o robô ja está ligado, o que deve dizer se ele pode começar a funcionar é o microstart portanto, inicializamos ele como stop 
-    this->pin = _state::STOP;
+    this->pin = pin;
+    this->state = ms_State::STOP;
 }
 
 void Microstarter::update() {
   //checar se está recebendo sinal do controle ou não para ligar
-    if (digitalRead(int pin)) 
-        this->state = _state::START;
+    if (digitalRead(this->pin)) 
+        this->state = ms_State::START;
     else   
-        this->state = _state::STOP;
-    
+        this->state = ms_State::STOP;
+
 }
